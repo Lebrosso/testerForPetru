cb96a550b9baa223775f71ef418219e0163ad7f0
JacekS <jsapieja@yahoo.com>
2021-07-21 23:26:14 +0200

    first working connection, two empty messages put into the queue

--- src/main/java/MqConnector.java
+++ src/main/java/MqConnector.java
@@ -1,26 +1,27 @@
 import com.ibm.mq.MQException;
 import com.ibm.mq.MQMessage;
 import com.ibm.mq.MQQueueManager;
+
+import java.io.IOException;
 import java.util.Hashtable;
 
+/**
+ * @author jsapieja
+ * Connector class using java classes for IBMMQ
+ */
 public class MqConnector {
     //MQMT_   flaga decydująca jaki message idzie
-    public MqConnector() throws MQException {
+    public MqConnector() throws MQException, IOException {
+
         Hashtable props = new Hashtable();
+
         props.put("service port", "1414");
         props.put("host name", "127.0.0.1");
-        MQQueueManager man = new MQQueueManager("testManager", props);
-        MQMessage mes = new MQMessage();
-        man.put("testQueue", mes);
-    }
 
-  /*  WebClient client = WebClient.create();
-    MQQueueManager queueManager;
-    {
-        try {
-            queueManager = new MQQueueManager("testManager");
-        } catch (MQException e) {
-            e.printStackTrace();
-        }
-    }  */
+        MQMessage mqMessage = new MQMessage();
+        mqMessage.writeString("trst");
+
+        MQQueueManager mqQueueManager = new MQQueueManager("testManager", props);
+        mqQueueManager.put("testQueue", mqMessage);
+    }
 }
--- src/main/java/TesterGui.java
+++ src/main/java/TesterGui.java
@@ -1,23 +1,26 @@
 import com.ibm.mq.MQException;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
 
+  /**
+  /  @author jsapieja
+  /  GUI for mq tester
+ */
 public class TesterGui {
 
     private MqConnector client;
     private JFrame mainFrame;
-    private JLabel headerLabel;
+    private JLabel messageTypeLabel;
     private JLabel locationLabel;
-    private JPanel controlPanel;
 
       public TesterGui(){
           mainFrame = new JFrame("IBMMQ tester");
-          mainFrame.setSize(400,400);
+          mainFrame.setSize(400,800);
           mainFrame.setLayout(new GridLayout(3, 1));
 
           mainFrame.addWindowListener(new WindowAdapter() {
@@ -26,23 +29,16 @@ public class TesterGui {
               }
           });
 
-          headerLabel = new JLabel("Choose the message type", JLabel.CENTER);
-          locationLabel = new JLabel("Type in the ibmmq server location", JLabel.CENTER);
+          messageTypeLabel = new JLabel("Choose the message type", JLabel.CENTER);
 
-          JTextField txtField = new JTextField(20);
+          locationLabel = new JLabel("Type in the ibmmq server location", JLabel.CENTER);
+          locationLabel.setSize(350,100);
+
+          JTextField serverLocation = new JTextField(20);
           String[] messageTypes = { MessageType.DATAGRAM.name(), MessageType.REPORT.name(), MessageType.REQUEST.name(), MessageType.REPLY.name(), MessageType.RESPONSE.name() };
 
           JComboBox messageList = new JComboBox(messageTypes);
           messageList.setSelectedIndex(4);
-
-        /*  “host name” - the name of the IBM MQ server
-        “service port” - the connection port
-        “channel name” - the element used to transfer messages between queues
-        “queue manager name” - the name of the element (it’s a logical entity) that handles the connection with the tested queue
-        “queue name” - the specific queue we are testing, our target of message exchanging
-        “userid” and “password” needed to open the connection  */
-
-          locationLabel.setSize(350,100);
 
           JButton sendButton = new JButton("Send message");
           sendButton.addActionListener(new ActionListener(){
@@ -51,18 +47,18 @@ public class TesterGui {
                       new MqConnector();
                   } catch (MQException mqException) {
                       mqException.printStackTrace();
+                  } catch (IOException ioException) {
+                      ioException.printStackTrace();
                   }
               }
           });
 
-          controlPanel = new JPanel();
-          controlPanel.setLayout(new FlowLayout());
+          mainFrame.add(messageTypeLabel);
+          mainFrame.add(messageList);
 
-          mainFrame.add(headerLabel);
-          mainFrame.add(controlPanel);
-          mainFrame.add(messageList);
           mainFrame.add(locationLabel);
-          mainFrame.add(messageList);
+          mainFrame.add(serverLocation);
+
           mainFrame.add(sendButton);
           mainFrame.setVisible(true);
       }
